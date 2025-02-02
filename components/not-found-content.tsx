929c456c9a14e014b28fcf49333ab67f9d4f6af7
NIL2000 <nilabhra09b.net@gmail.com>
2025-02-02 19:02:07 +0530

    Added animation to not-found view

--- components/not-found-content.tsx
+++ components/not-found-content.tsx
@@ -2,15 +2,33 @@
 
 import Link from "next/link";
 import React from "react";
+import * as motion from "motion/react-client";
 
 export default function NotFoundContent() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-4 p-4 h-full mt-10">
-      <h1 className="text-4xl">🫠</h1>
-      <h2 className="font-bold font-dm_serif text-5xl">
-        Sorry That page doesn’t exist.{" "}
-      </h2>
-      <p className="text-muted-foreground">
+    <div className="flex flex-col sm:items-center sm:justify-center justify-start space-y-4 p-4 h-[calc(100vh-8rem)]">
+      <motion.div
+        className="text-4xl"
+        initial={{ opacity: 0, translateY: 10 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        🫠
+      </motion.div>
+      <motion.div
+        className="font-bold font-dm_serif text-5xl"
+        initial={{ opacity: 0, translateY: 10 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        Sorry This page doesn’t exist.{" "}
+      </motion.div>
+      <motion.div
+        className="text-muted-foreground"
+        initial={{ opacity: 0, translateY: 10 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+      >
         Head to our{" "}
         <Link
           href={"/"}
@@ -19,7 +37,7 @@ export default function NotFoundContent() {
           homepage
         </Link>{" "}
         that does exist, or try double-checking the URL.
-      </p>
+      </motion.div>
     </div>
   );
 }
